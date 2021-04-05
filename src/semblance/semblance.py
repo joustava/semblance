af85657f2ae71f72f4d91ff9085c5a48285c9c86
joustava <joustava@gmail.com>
2021-04-05 15:11:08 +0200

    add feature label in image

--- src/semblance/semblance.py
+++ src/semblance/semblance.py
@@ -26,7 +26,13 @@ canny = CannyEdgeDetector()
 scanner = PDFScanner()
 tracker = TennisBallDetector()
 measure = SizeDetector()
-detectors = [measure, scanner, canny, faces, tracker]
+detectors = [
+    (measure, "Measures"),
+    (scanner, "Doc Scan"), 
+    (canny, "Canny Edges"),
+    (faces, "Face Detection"),
+    (tracker, "Ball Tracker")
+]
 
 class Semblance(object):
     def __init__(self, port=0, directory="./tmp"):
@@ -35,6 +41,7 @@ class Semblance(object):
         self._windowManager = WindowManager('Semblance', self._onKeyPress)
         self._selection = 0
         self._overlay = False
+        self._mirror = False
         self._take = False
         if port == 0:
             self._stream = WebcamVideoStream(0).start()
@@ -46,9 +53,13 @@ class Semblance(object):
         while self._windowManager.isWindowCreated:
             frame = self._stream.read()
             if frame is not None:
+                # if(self._mirror):
+                #     frame = np.fliplr(frame)
+                    
+                feature = detectors[self._selection]
+                _frame = feature[0].detect(frame)
                 
-                _frame = detectors[self._selection].detect(frame)
-                
+                _frame = self.add_label(_frame, feature[1])
                 self._windowManager.show(_frame)
                 if(self._take):
                     self.snapshot(_frame)
@@ -65,12 +76,22 @@ class Semblance(object):
             self._cycle_choice()
         if keycode == ord("o"):
             self._overlay = not self._overlay
+        if keycode == ord("m"):
+            self._mirror = not self._mirror
 
     def snapshot(self, frame):
         path = "~/Downloads/snapshot_" + dt.datetime.today().isoformat() + ".jpg"
         path = os.path.expanduser(path)
         cv2.imwrite(path, frame)
 
+    def add_label(self, frame, text):
+        (h, w) = frame.shape[:2]
+
+        
+        cv2.putText(frame, text, (20, h-20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        
+        return frame
+    
     def _cycle_choice(self):
         if(self._selection < len(detectors) - 1):
             self._selection += 1
